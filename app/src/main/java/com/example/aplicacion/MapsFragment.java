1f72f33f2cc9e00ed9c38233a4e054d9eed4f5ba
lizette5 <lquispeflore@unsa.edu.pe>
2020-12-02 22:52:47 -0500

    Laboratorio 09

---
+++ app/src/main/java/com/example/aplicacion/MapsFragment.java
@@ -0,0 +1,301 @@
+package com.example.aplicacion;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Service;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.aplicacion.Presenter.Servicios.MiServicioGPS;
+import com.example.aplicacion.View.Utilidades;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.android.volley.RequestQueue;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MapsFragment extends Fragment {
+
+    GoogleMap mMap;
+    MiServicioGPS miServicio;
+    double longitud;
+    double latitud;
+    public static Location location = null;
+    boolean gpsActivo;
+    LocationManager locationManager;
+    TextView texto;
+
+    JsonObjectRequest jsonObjectRequest;
+    RequestQueue request;
+
+    FusedLocationProviderClient fusedLocationProviderClient;
+
+    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            /*LatLng sydney = new LatLng(-34, 151);
+            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+            mMap = googleMap;
+
+
+            if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            mMap.setMyLocationEnabled(true);
+            miServicio = new MiServicioGPS(getActivity().getApplicationContext(), mMap);
+
+
+
+            /*LatLng center = null;
+            ArrayList<LatLng> points = null;
+            PolylineOptions lineOptions = null;
+
+            // setUpMapIfNeeded();
+
+            // recorriendo todas las rutas
+            for(int i=0;i<Utilidades.routes.size();i++){
+                points = new ArrayList<LatLng>();
+                lineOptions = new PolylineOptions();
+
+                // Obteniendo el detalle de la ruta
+                List<HashMap<String, String>> path = Utilidades.routes.get(i);
+
+                // Obteniendo todos los puntos y/o coordenadas de la ruta
+                for(int j=0;j<path.size();j++){
+                    HashMap<String,String> point = path.get(j);
+
+                    double lat = Double.parseDouble(point.get("lat"));
+                    double lng = Double.parseDouble(point.get("lng"));
+                    LatLng position = new LatLng(lat, lng);
+
+                    if (center == null) {
+                        //Obtengo la 1ra coordenada para centrar el mapa en la misma.
+                        center = new LatLng(lat, lng);
+                    }
+                    points.add(position);
+                }
+
+                // Agregamos todos los puntos en la ruta al objeto LineOptions
+                lineOptions.addAll(points);
+                //Definimos el grosor de las Polilíneas
+                lineOptions.width(2);
+                //Definimos el color de la Polilíneas
+                lineOptions.color(Color.BLUE);
+            }
+
+            // Dibujamos las Polilineas en el Google Map para cada ruta
+            mMap.addPolyline(lineOptions);
+
+            LatLng origen = new LatLng(Utilidades.coordenadas.getLatitudInicial(), Utilidades.coordenadas.getLongitudInicial());
+            mMap.addMarker(new MarkerOptions().position(origen).title("Lat: "+Utilidades.coordenadas.getLatitudInicial()+" - Long: "+Utilidades.coordenadas.getLongitudInicial()));
+
+            LatLng destino = new LatLng(Utilidades.coordenadas.getLatitudFinal(), Utilidades.coordenadas.getLongitudFinal());
+            mMap.addMarker(new MarkerOptions().position(destino).title("Lat: "+Utilidades.coordenadas.getLatitudFinal()+" - Long: "+Utilidades.coordenadas.getLongitudFinal()));
+
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(center, 15));
+            /////////////
+
+        }*/
+        }
+    }
+
+        ;
+
+        @Nullable
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater,
+                                 @Nullable ViewGroup container,
+                                 @Nullable Bundle savedInstanceState) {
+            //prueba = (TextView
+            View root = inflater.inflate(R.layout.fragment_maps, container, false);
+            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+            @SuppressLint("MissingPermission") Task<Location> locationTask=fusedLocationProviderClient.getLastLocation();
+            locationTask.addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if(location !=null){
+                        location.getLongitude();
+                        location.getLatitude();
+                    }
+                }
+            });
+        return root;
+
+        }
+
+        @Override
+        public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            SupportMapFragment mapFragment =
+                    (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+            if (mapFragment != null) {
+                mapFragment.getMapAsync(callback);
+            }
+
+            //miServicio = new MiServicioGPS(getActivity().getApplicationContext(), mMap);
+        }
+
+}
+
+//ADD
+  /*  private void webServiceObtenerRuta(String latitudInicial, String longitudInicial, String latitudFinal, String longitudFinal) {
+
+        String url="https://maps.googleapis.com/maps/api/directions/json?origin="+latitudInicial+","+longitudInicial
+                +"&destination="+latitudFinal+","+longitudFinal;
+
+        jsonObjectRequest=new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                //Este método PARSEA el JSONObject que retorna del API de Rutas de Google devolviendo
+                //una lista del lista de HashMap Strings con el listado de Coordenadas de Lat y Long,
+                //con la cual se podrá dibujar pollinas que describan la ruta entre 2 puntos.
+                JSONArray jRoutes = null;
+                JSONArray jLegs = null;
+                JSONArray jSteps = null;
+
+                try {
+
+                    jRoutes = response.getJSONArray("routes");
+
+                    /** Traversing all routes */
+    /*
+                    for(int i=0;i<jRoutes.length();i++){
+                        jLegs = ( (JSONObject)jRoutes.get(i)).getJSONArray("legs");
+                        List<HashMap<String, String>> path = new ArrayList<HashMap<String, String>>();
+
+                        /** Traversing all legs */
+    /*
+                        for(int j=0;j<jLegs.length();j++){
+                            jSteps = ( (JSONObject)jLegs.get(j)).getJSONArray("steps");
+
+                            /** Traversing all steps */
+    /*
+                            for(int k=0;k<jSteps.length();k++){
+                                String polyline = "";
+                                polyline = (String)((JSONObject)((JSONObject)jSteps.get(k)).get("polyline")).get("points");
+                                List<LatLng> list = decodePoly(polyline);
+
+                                /** Traversing all points */
+    /*
+                                for(int l=0;l<list.size();l++){
+                                    HashMap<String, String> hm = new HashMap<String, String>();
+                                    hm.put("lat", Double.toString(((LatLng)list.get(l)).latitude) );
+                                    hm.put("lng", Double.toString(((LatLng)list.get(l)).longitude) );
+                                    path.add(hm);
+                                }
+                            }
+                            Utilidades.routes.add(path);
+                        }
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }catch (Exception e){
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getActivity().getApplicationContext(), "No se puede conectar "+error.toString(), Toast.LENGTH_LONG).show();
+                System.out.println();
+                Log.d("ERROR: ", error.toString());
+            }
+        }
+        );
+
+        request.add(jsonObjectRequest);
+    }
+
+    private List<LatLng> decodePoly(String encoded) {
+
+        List<LatLng> poly = new ArrayList<LatLng>();
+        int index = 0, len = encoded.length();
+        int lat = 0, lng = 0;
+
+        while (index < len) {
+            int b, shift = 0, result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+
+            shift = 0;
+            result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+
+            LatLng p = new LatLng((((double) lat / 1E5)),
+                    (((double) lng / 1E5)));
+            poly.add(p);
+        }
+
+        return poly;
+    }
+*/
---
+++ app/src/main/java/com/example/aplicacion/Presenter/Servicios/MiServicioGPS.java
@@ -0,0 +1,341 @@
+package com.example.aplicacion.Presenter.Servicios;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.aplicacion.MapsFragment;
+import com.example.aplicacion.View.Utilidades;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MiServicioGPS extends Service implements LocationListener {
+    private final Context contex;
+    public double longitud;
+    public double latitud;
+    //public Location location;
+    boolean gpsActivo;
+    LocationManager locationManager;
+    TextView texto;
+    GoogleMap mMap;
+    boolean flag1 = true;
+    double latinicial, longinicial;
+
+    JsonObjectRequest jsonObjectRequest;
+    RequestQueue request;
+    int i =0;
+
+    public MiServicioGPS() {
+        super();
+        this.contex = getApplicationContext();
+    }
+
+    public MiServicioGPS(Context contex, GoogleMap mMap) {
+        super();
+        this.contex = contex;
+        this.mMap = mMap;
+        request= Volley.newRequestQueue(contex);
+        getLocation();
+
+    }
+
+    @SuppressLint("MissingPermission")
+    public void getLocation() {
+        try {
+            locationManager = (LocationManager) this.contex.getSystemService(LOCATION_SERVICE);
+            gpsActivo = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        } catch (Exception e) {
+        }if (gpsActivo) {
+            /*if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }*/
+            locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER
+                    , 1000 * 10
+                    , 10, this);//peticion al servicio para obtener actualizaciones del gps cada cierto tiempo
+            MapsFragment.location  =  locationManager.getLastKnownLocation(locationManager.GPS_PROVIDER);
+            latitud =  MapsFragment.location.getLatitude();
+            longitud = MapsFragment.location.getLongitude();
+            Toast.makeText(contex, "Ingreso", Toast.LENGTH_LONG ).show();
+
+
+
+
+
+
+        }
+
+    }
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        //texto.setText("Coordenadas: "+location.getLatitude() + " / "+ location.getLongitude());
+        Toast.makeText(contex, "Coordenadas: "+location.getLatitude() + " / "+ location.getLatitude()+ "**"+ ++i, Toast.LENGTH_LONG ).show();
+        LatLng tmp = new LatLng( location.getLatitude(),location.getLongitude());
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(tmp));
+        /*latitud =  location.getLatitude();
+        longitud = location.getLongitude();
+        if(flag1){
+            latinicial=location.getLatitude();
+            longinicial=location.getLongitude();
+            flag1=false;
+            return;
+        }*/
+        //webServiceObtenerRuta(String.valueOf(latinicial), String.valueOf(longinicial),String.valueOf(location.getLatitude()),String.valueOf(location.getLongitude()));
+        //draw();
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+
+    }
+    public void setView(View view){
+        texto = (TextView)view;
+    }
+    private void webServiceObtenerRuta(String latitudInicial, String longitudInicial, String latitudFinal, String longitudFinal) {
+
+        String url="https://maps.googleapis.com/maps/api/directions/json?origin="+latitudInicial+","+longitudInicial
+                +"&destination="+latitudFinal+","+longitudFinal;
+
+        jsonObjectRequest=new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                //Este método PARSEA el JSONObject que retorna del API de Rutas de Google devolviendo
+                //una lista del lista de HashMap Strings con el listado de Coordenadas de Lat y Long,
+                //con la cual se podrá dibujar pollinas que describan la ruta entre 2 puntos.
+                JSONArray jRoutes = null;
+                JSONArray jLegs = null;
+                JSONArray jSteps = null;
+
+                try {
+
+                    jRoutes = response.getJSONArray("routes");
+
+                    /** Traversing all routes */
+                    for(int i=0;i<jRoutes.length();i++){
+                        jLegs = ( (JSONObject)jRoutes.get(i)).getJSONArray("legs");
+                        List<HashMap<String, String>> path = new ArrayList<HashMap<String, String>>();
+
+                        /** Traversing all legs */
+                        for(int j=0;j<jLegs.length();j++){
+                            jSteps = ( (JSONObject)jLegs.get(j)).getJSONArray("steps");
+
+                            /** Traversing all steps */
+                            for(int k=0;k<jSteps.length();k++){
+                                String polyline = "";
+                                polyline = (String)((JSONObject)((JSONObject)jSteps.get(k)).get("polyline")).get("points");
+                                List<LatLng> list = decodePoly(polyline);
+
+                                /** Traversing all points */
+                                for(int l=0;l<list.size();l++){
+                                    HashMap<String, String> hm = new HashMap<String, String>();
+                                    hm.put("lat", Double.toString(((LatLng)list.get(l)).latitude) );
+                                    hm.put("lng", Double.toString(((LatLng)list.get(l)).longitude) );
+                                    path.add(hm);
+                                }
+                            }
+                            Utilidades.routes.add(path);
+                        }
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }catch (Exception e){
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(contex.getApplicationContext(), "No se puede conectar "+error.toString(), Toast.LENGTH_LONG).show();
+                System.out.println();
+                Log.d("ERROR: ", error.toString());
+            }
+        }
+        );
+
+        request.add(jsonObjectRequest);
+    }
+
+    private List<LatLng> decodePoly(String encoded) {
+
+        List<LatLng> poly = new ArrayList<LatLng>();
+        int index = 0, len = encoded.length();
+        int lat = 0, lng = 0;
+
+        while (index < len) {
+            int b, shift = 0, result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+
+            shift = 0;
+            result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+
+            LatLng p = new LatLng((((double) lat / 1E5)),
+                    (((double) lng / 1E5)));
+            poly.add(p);
+        }
+
+        return poly;
+    }
+
+    public void draw(){
+
+
+        LatLng center = null;
+        ArrayList<LatLng> points = null;
+        PolylineOptions lineOptions = null;
+
+        // setUpMapIfNeeded();
+
+        // recorriendo todas las rutas
+        for(int i = 0; i< Utilidades.routes.size(); i++){
+            points = new ArrayList<LatLng>();
+            lineOptions = new PolylineOptions();
+
+            // Obteniendo el detalle de la ruta
+            List<HashMap<String, String>> path = Utilidades.routes.get(i);
+
+            // Obteniendo todos los puntos y/o coordenadas de la ruta
+            for(int j=0;j<path.size();j++){
+                HashMap<String,String> point = path.get(j);
+
+                double lat = Double.parseDouble(point.get("lat"));
+                double lng = Double.parseDouble(point.get("lng"));
+                LatLng position = new LatLng(lat, lng);
+
+                if (center == null) {
+                    //Obtengo la 1ra coordenada para centrar el mapa en la misma.
+                    center = new LatLng(lat, lng);
+                }
+                points.add(position);
+            }
+
+            // Agregamos todos los puntos en la ruta al objeto LineOptions
+            lineOptions.addAll(points);
+            //Definimos el grosor de las Polilíneas
+            lineOptions.width(2);
+            //Definimos el color de la Polilíneas
+            lineOptions.color(Color.RED);
+        }
+
+        // Dibujamos las Polilineas en el Google Map para cada ruta
+        mMap.addPolyline(lineOptions);
+
+        LatLng origen = new LatLng(Utilidades.coordenadas.getLatitudInicial(), Utilidades.coordenadas.getLongitudInicial());
+        mMap.addMarker(new MarkerOptions().position(origen).title("Lat: "+Utilidades.coordenadas.getLatitudInicial()+" - Long: "+Utilidades.coordenadas.getLongitudInicial()));
+
+        LatLng destino = new LatLng(Utilidades.coordenadas.getLatitudFinal(), Utilidades.coordenadas.getLongitudFinal());
+        mMap.addMarker(new MarkerOptions().position(destino).title("Lat: "+Utilidades.coordenadas.getLatitudFinal()+" - Long: "+Utilidades.coordenadas.getLongitudFinal()));
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(center, 15));
+        /////////////
+    }
+
+    public List<List<HashMap<String,String>>> parse(JSONObject jObject){
+        //Este método PARSEA el JSONObject que retorna del API de Rutas de Google devolviendo
+        //una lista del lista de HashMap Strings con el listado de Coordenadas de Lat y Long,
+        //con la cual se podrá dibujar pollinas que describan la ruta entre 2 puntos.
+        JSONArray jRoutes = null;
+        JSONArray jLegs = null;
+        JSONArray jSteps = null;
+
+        try {
+
+            jRoutes = jObject.getJSONArray("routes");
+
+            /** Traversing all routes */
+            for(int i=0;i<jRoutes.length();i++){
+                jLegs = ( (JSONObject)jRoutes.get(i)).getJSONArray("legs");
+                List<HashMap<String, String>> path = new ArrayList<HashMap<String, String>>();
+
+                /** Traversing all legs */
+                for(int j=0;j<jLegs.length();j++){
+                    jSteps = ( (JSONObject)jLegs.get(j)).getJSONArray("steps");
+
+                    /** Traversing all steps */
+                    for(int k=0;k<jSteps.length();k++){
+                        String polyline = "";
+                        polyline = (String)((JSONObject)((JSONObject)jSteps.get(k)).get("polyline")).get("points");
+                        List<LatLng> list = decodePoly(polyline);
+
+                        /** Traversing all points */
+                        for(int l=0;l<list.size();l++){
+                            HashMap<String, String> hm = new HashMap<String, String>();
+                            hm.put("lat", Double.toString(((LatLng)list.get(l)).latitude) );
+                            hm.put("lng", Double.toString(((LatLng)list.get(l)).longitude) );
+                            path.add(hm);
+                        }
+                    }
+                    Utilidades.routes.add(path);
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }catch (Exception e){
+        }
+        return Utilidades.routes;
+    }
+
+}
